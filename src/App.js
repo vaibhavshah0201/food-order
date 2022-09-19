064b4472252373b78c3f581b749e53a1f2caf125
Vaibhav Shah <vshah@cyzma.com>
2022-09-19 18:36:51 +0530

    cart modal added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
+import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  }
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  }
   return (
     <Fragment>
+      {cartIsShown && <Cart />}
       <Header></Header>
       <main>
         <Meals></Meals>
